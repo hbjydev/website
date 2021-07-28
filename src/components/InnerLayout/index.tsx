b417b4db055f97502cb8f39649dfcba75e4b0318
Hayden Young <hi@hbjy.dev>
2021-07-28 19:35:45 +0100

    fix: resolve issue with page height

--- src/components/InnerLayout/index.tsx
+++ src/components/InnerLayout/index.tsx
@@ -9,7 +9,7 @@ type InnerLayoutProps = {
 
 const InnerLayout = ({ children, leftColumn, rightColumn }: InnerLayoutProps) => {
     return (
-        <div className="grid grid-rows-1 md:grid-rows-4 lg:grid-rows-5 lg:gap-1 flex-grow" id="innerlayout_container">
+        <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-5 lg:gap-1 flex-grow" id="innerlayout_container">
             { leftColumn ? (
                 <div className="col-span-1 h-full bg-gray-200 dark:bg-gray-800 border-b md:border-b-0 md:border-r border-gray-400 dark:border-gray-700" id="layout_col_left">
                     {leftColumn}
